ffb961e9ff9f922789b1f34fb2ff266c4b7c3193
Krystian Kat <krystian.kat@protonmail.com>
2022-04-04 09:56:59 +0200

    refactor: update IEmail name

--- interfaces/email.ts
+++ interfaces/email.ts
@@ -1,3 +1,27 @@
-export interface ISendEmail {
+export interface IEmail {
   to: string;
 }
+
+// export interface ISendEmail {
+//   send(to?: ISendEmail): Promise<any>;
+// }
+// export class SendEmail implements ISearchMethod {
+//   async search(request: ISendEmail): Promise<Results> {
+//     console.log('SearchMethod - request', request);
+//     // try {
+//     const response = await fetch(
+//       'http://localhost:3000/api/main?' +
+//         new URLSearchParams({
+//           send: request.to || '',
+//         })
+//     );
+//     const data = await response.json();
+//     console.log('SearchMethod data', data);
+
+//     // return await response.json();
+//     return data;
+//     // } catch (err) {
+//     //   console.error(err);
+//     // }
+//   }
+// }
