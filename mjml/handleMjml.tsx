65580e7dacf09fd4e622b40b1d69f6a42777a223
Monika Bugajewska <bugajewska.m@gmail.com>
2022-04-05 00:51:23 +0200

    Update handleMjml

--- mjml/handleMjml.tsx
+++ mjml/handleMjml.tsx
@@ -5,51 +5,96 @@ import {
   MjmlSection,
   MjmlColumn,
   MjmlButton,
-} from 'mjml-react';
+  MjmlImage,
+  MjmlText,
+  MjmlWrapper,
+} from "mjml-react";
+import { image } from "./images";
 
 export function renderHtml(email: string) {
-  const { html, _ } = render(
+  const { html, } = render(
     <Mjml>
-      <MjmlBody>
-        <MjmlSection>
-            {[1, 2, 3, 4, 5].map((number)=>
-          <MjmlColumn>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=${number}`}
-            >
-              {number}
-            </MjmlButton>
+      <MjmlBody background-color="#eee" width={600}>
+        <MjmlSection
+          full-width="full-width"
+          background-color="#00ffa3"
+          padding-bottom="0"
+        >
+          <MjmlColumn width="100%">
+            <MjmlImage
+              src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v2hl0uhrznxcbhdpk6if"
+              alt=""
+              align="center"
+              width="150px"
+            />
           </MjmlColumn>
-            )}
+          <MjmlColumn background-color="#fff" width="600px">
+            <MjmlText
+              color="#212b35"
+              font-weight="bold"
+              font-size="20px"
+              padding-top="30px"
+              padding-bottom="16px"
+              align="center"
+            >
+              Daj nam znać jak oceniasz Bootcamp!
+            </MjmlText>
+          </MjmlColumn>
         </MjmlSection>
+        <MjmlWrapper padding-top="0" padding-bottom="0" css-class="body-section">
+          <MjmlSection background-color="#ffffff">
+            <MjmlColumn width="100%">
+              <MjmlImage
+              src={image}
+              alt=""
+              align="center"
+              width="250px"
+            />
+              <MjmlText color="#637381" font-size="16px" align="center">
+                Naciśnij na jeden z przycisków aby ocenić!
+              </MjmlText>
+            </MjmlColumn>
+          </MjmlSection>
+          <MjmlSection background-color="#ffffff">
+            {[1, 2, 3, 4, 5].map((number) => (
+              <MjmlColumn>
+                <MjmlButton
+                  font-family="Helvetica"
+                  background-color="#00ffa3"
+                  color="black"
+                  href={`http://localhost:3000/api/email?email=${email}&vote=${number}`}
+                >
+                  {number}
+                </MjmlButton>
+              </MjmlColumn>
+            ))}
+          </MjmlSection>
+        </MjmlWrapper>
       </MjmlBody>
     </Mjml>,
-    { validationLevel: 'soft' }
+    { validationLevel: "soft" }
   );
 
   return html;
 }
 
-// import mjml2html from 'mjml';
+// import Mjml2html from 'Mjml';
 
 // /*
-//   Compile an mjml string
+//   Compile an Mjml string
 // */
-// const htmlOutput = mjml2html(
+// const htmlOutput = Mjml2html(
 //   `
-//   <mjml>
-//     <mj-body>
-//       <mj-section>
-//         <mj-column>
-//           <mj-text>
+//   <Mjml>
+//     <MjmlBody>
+//       <MjmlSection>
+//         <MjmlColumn>
+//           <MjmlText>
 //             Hello World!
-//           </mj-text>
-//         </mj-column>
-//       </mj-section>
-//     </mj-body>
-//   </mjml>
+//           </MjmlText>
+//         </MjmlColumn>
+//       </MjmlSection>
+//     </MjmlBody>
+//   </Mjml>
 // `
 // );
