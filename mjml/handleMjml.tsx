5371042874982ed7cad3886ac3391e9e950c7167
Monika Bugajewska <bugajewska.m@gmail.com>
2022-04-04 23:16:26 +0200

    use map for buttons, change email layout

--- mjml/handleMjml.tsx
+++ mjml/handleMjml.tsx
@@ -12,48 +12,18 @@ export function renderHtml(email: string) {
     <Mjml>
       <MjmlBody>
         <MjmlSection>
+            {[1, 2, 3, 4, 5].map((number)=>
           <MjmlColumn>
             <MjmlButton
               font-family="Helvetica"
               background-color="#f45e43"
               color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=1`}
+              href={`http://localhost:3000/api/email?email=${email}&vote=${number}`}
             >
-              1
-            </MjmlButton>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=2`}
-            >
-              2
-            </MjmlButton>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=3`}
-            >
-              3
-            </MjmlButton>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=4`}
-            >
-              4
-            </MjmlButton>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=5`}
-            >
-              5
+              {number}
             </MjmlButton>
           </MjmlColumn>
+            )}
         </MjmlSection>
       </MjmlBody>
     </Mjml>,
@@ -63,23 +33,23 @@ export function renderHtml(email: string) {
   return html;
 }
 
-import mjml2html from 'mjml';
+// import mjml2html from 'mjml';
 
-/*
-  Compile an mjml string
-*/
-const htmlOutput = mjml2html(
-  `
-  <mjml>
-    <mj-body>
-      <mj-section>
-        <mj-column>
-          <mj-text>
-            Hello World!
-          </mj-text>
-        </mj-column>
-      </mj-section>
-    </mj-body>
-  </mjml>
-`
-);
+// /*
+//   Compile an mjml string
+// */
+// const htmlOutput = mjml2html(
+//   `
+//   <mjml>
+//     <mj-body>
+//       <mj-section>
+//         <mj-column>
+//           <mj-text>
+//             Hello World!
+//           </mj-text>
+//         </mj-column>
+//       </mj-section>
+//     </mj-body>
+//   </mjml>
+// `
+// );
