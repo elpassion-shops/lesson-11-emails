1bf43d958bde8bfcf60e4a8697cbe1a13781f503
Klaudiusz <d1f1.6@protonmail.com>
2022-04-04 19:07:37 +0200

    Code reformatted with Prettier

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,11 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import nodemailer from 'nodemailer';
-import nodemailerSendgrid from 'nodemailer-sendgrid';
-import { IEmailMsg, IEmailVote } from '../../interfaces/email';
-import { config } from '../../config/configuration';
-import { Client } from 'pg';
-import {renderHtml} from "../../mjml/handleMjml";
-import {VoteDbClient} from "../../services/postgresClient";
+import type { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
+import nodemailerSendgrid from "nodemailer-sendgrid";
+import { IEmailMsg, IEmailVote } from "../../interfaces/email";
+import { config } from "../../config/configuration";
+import { Client } from "pg";
+import { renderHtml } from "../../mjml/handleMjml";
+import { VoteDbClient } from "../../services/databaseService";
 
 const sendEmail = async (msg: IEmailMsg) => {
   const transporter = nodemailer.createTransport(
@@ -16,46 +16,51 @@ const sendEmail = async (msg: IEmailMsg) => {
   return await transporter.sendMail(msg);
 };
 
+function createClient(): Client {
+  return new Client({
+    user: config.database.username,
+    host: config.database.host,
+    database: config.database.database,
+    password: config.database.password,
+    port: config.database.port,
+  });
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     const data: IEmailVote = {
       email: req.query.email as string,
       vote: Number(req.query.vote),
     };
-    const client = new Client({
-      user: config.database.username,
-      host: config.database.host,
-      database: config.database.database,
-      password: config.database.password,
-      port: config.database.port,
-    });
+    const client = new Client(createClient());
     const voteClient = new VoteDbClient(client);
-    await voteClient.connect()
+    await voteClient.connect();
     await voteClient.createTable();
     await voteClient.insertVote(data.email, data.vote);
     const vote = await voteClient.getVote(data.email);
-    await voteClient.end()
-    console.log(vote)
+    const average = await voteClient.getAverageVote();
+    await voteClient.end();
     res.status(200).json({
-      message: 'Vote saved',
+      message: "Vote saved",
       vote: vote.rows,
+      averageVote: average,
     });
   }
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const html = renderHtml(req.body.to);
     const msg: IEmailMsg = {
       to: req.body.to,
-      from: 'klaudiusz.witt@gmail.com',
-      subject: 'BrrBrrBru',
+      from: "klaudiusz.witt@gmail.com",
+      subject: "Vote please!",
       html: html,
     };
     try {
       await sendEmail(msg);
       res.status(200).json({
-        message: 'Email sent',
+        message: "Email sent",
       });
     } catch (e) {
       res.status(500).json({
--- services/databaseService.ts
+++ services/databaseService.ts
@@ -1,64 +1,90 @@
-import pg, {QueryResult} from 'pg';
+import pg, { QueryResult } from "pg";
 class DatabaseService {
-    private client: pg.Client;
-    constructor(user: string, password: string, host: string, port: number, database: string) {
-        this.client = new pg.Client({
-            user,
-            password,
-            host,
-            port,
-            database
-        });
-    }
-    public async connect(): Promise<void> {
-        await this.client.connect();
-    }
-    public async query(query: string, values?: any[]): Promise<pg.QueryResult> {
-        return this.client.query(query, values);
-    }
-    public async end(): Promise<void> {
-        await this.client.end();
-    }
+  private client: pg.Client;
+  constructor(
+    user: string,
+    password: string,
+    host: string,
+    port: number,
+    database: string
+  ) {
+    this.client = new pg.Client({
+      user,
+      password,
+      host,
+      port,
+      database,
+    });
+  }
+  public async connect(): Promise<void> {
+    await this.client.connect();
+  }
+  public async query(query: string, values?: any[]): Promise<pg.QueryResult> {
+    return this.client.query(query, values);
+  }
+  public async end(): Promise<void> {
+    await this.client.end();
+  }
 }
 
 export class VoteDbClient {
-    private client: DatabaseService;
-    constructor(client: any) {
-        this.client = new DatabaseService(client.user, client.password, client.host, client.port, client.database);
-    }
-    public async run() {
-        await this.client.connect();
-        await this.createTable();
-    }
-    public async connect(): Promise<void> {
-        await this.client.connect();
-    }
-    public async end(): Promise<void> {
-        await this.client.end();
-    }
-    public async createTable(): Promise<QueryResult> {
-        return await this.client.query(`CREATE TABLE IF NOT EXISTS votes (
+  private client: DatabaseService;
+  constructor(client: any) {
+    this.client = new DatabaseService(
+      client.user,
+      client.password,
+      client.host,
+      client.port,
+      client.database
+    );
+  }
+  public async run() {
+    await this.client.connect();
+    await this.createTable();
+  }
+  public async connect(): Promise<void> {
+    await this.client.connect();
+  }
+  public async end(): Promise<void> {
+    await this.client.end();
+  }
+  public async createTable(): Promise<QueryResult> {
+    return await this.client.query(`CREATE TABLE IF NOT EXISTS votes (
             id SERIAL PRIMARY KEY,
             email VARCHAR(255) NOT NULL,
             vote INT NOT NULL
         )`);
+  }
+  public async insertVote(email: string, vote: number): Promise<QueryResult> {
+    const result = await this.client.query(
+      `SELECT * FROM votes WHERE email = $1`,
+      [email]
+    );
+    if (result.rows.length > 0) {
+      return await this.updateVote(email, vote);
+    } else {
+      return this.client.query(
+        `INSERT INTO votes (email, vote) VALUES ($1, $2)`,
+        [email, vote]
+      );
     }
-    public async insertVote(email: string, vote: number): Promise<QueryResult> {
-        const result = await this.client.query(`SELECT * FROM votes WHERE email = $1`, [email]);
-        if (result.rows.length > 0) {
-            return await this.updateVote(email, vote);
-        } else {
-            return this.client.query(`INSERT INTO votes (email, vote) VALUES ($1, $2)`, [email, vote]);
-        }
-    }
-    public async updateVote(email: string, vote: number): Promise<pg.QueryResult> {
-        return await this.client.query(`UPDATE votes SET vote = $1 WHERE email = $2`, [vote, email]);
-    }
-    public async getVote(email: string): Promise<pg.QueryResult> {
-        return await this.client.query(`SELECT * FROM votes WHERE email = '${email}'`);
-    }
+  }
+  public async updateVote(
+    email: string,
+    vote: number
+  ): Promise<pg.QueryResult> {
+    return await this.client.query(
+      `UPDATE votes SET vote = $1 WHERE email = $2`,
+      [vote, email]
+    );
+  }
+  public async getVote(email: string): Promise<pg.QueryResult> {
+    return await this.client.query(
+      `SELECT * FROM votes WHERE email = '${email}'`
+    );
+  }
 
-    public async getAverageVote(): Promise<pg.QueryResult> {
-        return await this.client.query(`SELECT AVG(vote) FROM votes`);
-    }
+  public async getAverageVote(): Promise<pg.QueryResult> {
+    return await this.client.query(`SELECT AVG(vote) FROM votes`);
+  }
 }
