01d12f490ccaa7f02c253138cd13cd7b2ac98312
Krystian Kat <krystian.kat@protonmail.com>
2022-04-05 00:46:25 +0200

    feat: improve email styling in renderHtml

--- mjml/handleMjml.tsx
+++ mjml/handleMjml.tsx
@@ -1,58 +1,188 @@
 import {
   render,
   Mjml,
+  MjmlHead,
+  MjmlTitle,
+  MjmlPreview,
   MjmlBody,
   MjmlSection,
   MjmlColumn,
   MjmlButton,
+  MjmlImage,
+  MjmlStyle,
+  MjmlText,
+  MjmlClass,
+  MjmlAttributes,
+  MjmlAll,
+  MjmlHero,
+  MjmlGroup
 } from 'mjml-react';
+
 
 export function renderHtml(email: string) {
   const { html, _ } = render(
     <Mjml>
-      <MjmlBody>
+      <MjmlHead>
+        <MjmlAttributes>
+          <MjmlText padding="0" />
+          <MjmlClass name="elpassion" color="#00ffa3" />
+          <MjmlClass
+            name="button"
+            width="60px"
+            color="black"
+            background-color="#00ffa3"
+            font-family="Helvetica"
+            font-weight="900"
+          />
+          <MjmlClass name="main" color="black" />
+          <MjmlClass name="big" font-size="24px" />
+          <MjmlClass name="center" align="center" />
+          <MjmlAll font-family="Helvetica" />
+        </MjmlAttributes>
+      </MjmlHead>
+
+      <MjmlBody background-color="black">
+        <MjmlHero
+          height="479px"
+          background-width="800px"
+          background-height="469px"
+          background-url="https://www.elpassion.com/hubfs/0%20homepage%20assets/elp-home-banner@2x-2-2.webp"
+          background-color="#2a3448"
+          padding="40px 0px 10px"
+        >
+          <MjmlText
+            padding="20px"
+            color="#ffffff"
+            font-family="Helvetica"
+            align="center"
+            font-size="43px"
+            line-height="45px"
+            font-weight="900"
+          >
+            RATE WORKSHOP
+          </MjmlText>
+
+          <MjmlText
+            padding="20px"
+            color="#ffffff"
+            font-family="Helvetica"
+            align="center"
+            font-size="20px"
+            line-height="45px"
+            font-weight="900"
+            letter-spacing="3px"
+          >
+            TELL US THE WHOLE TRUTH
+          </MjmlText>
+
+          <MjmlText
+            padding="10px"
+            color="#ffffff"
+            font-family="Helvetica"
+            align="center"
+            font-size="12px"
+            line-height="45px"
+            font-weight="900"
+            letter-spacing="3px"
+          >
+            THIS IS YOUR ONLY CHANCE
+          </MjmlText>
+
+          <MjmlText
+            padding="10px"
+            color="#ffffff"
+            font-family="Helvetica"
+            align="center"
+            font-size="8px"
+            line-height="45px"
+            font-weight="900"
+            letter-spacing="3px"
+          >
+            THIS IS REQUIRED
+          </MjmlText>
+
+          <MjmlText
+            padding="8px"
+            color="#ffffff"
+            font-family="Helvetica"
+            align="center"
+            font-size="6px"
+            line-height="45px"
+            font-weight="900"
+            letter-spacing="3px"
+          >
+            DO IT NOW!
+          </MjmlText>
+        </MjmlHero>
+
+        <MjmlSection>
+          <MjmlGroup MjmlClass="center">
+            <MjmlColumn>
+              <MjmlButton
+                width="60px" color="black" background-color="#00ffa3" font-family="Helvetica" font-weight="900"
+                href={`http://localhost:3000/api/email?email=${email}&vote=1`}
+              >
+                1
+              </MjmlButton>
+            </MjmlColumn>
+
+            <MjmlColumn>
+              <MjmlButton
+                width="60px" color="black" background-color="#00ffa3" font-family="Helvetica" font-weight="900"
+                href={`http://localhost:3000/api/email?email=${email}&vote=2`}
+              >
+                2
+              </MjmlButton>
+            </MjmlColumn>
+
+            <MjmlColumn>
+              <MjmlButton
+                width="60px" color="black" background-color="#00ffa3" font-family="Helvetica" font-weight="900"
+                href={`http://localhost:3000/api/email?email=${email}&vote=3`}
+              >
+                3
+              </MjmlButton>
+            </MjmlColumn>
+
+            <MjmlColumn>
+              <MjmlButton
+                width="60px" color="black" background-color="#00ffa3" font-family="Helvetica" font-weight="900"
+                href={`http://localhost:3000/api/email?email=${email}&vote=4`}
+              >
+                4
+              </MjmlButton>
+            </MjmlColumn>
+
+            <MjmlColumn>
+              <MjmlButton
+                width="60px" color="black" background-color="#00ffa3" font-family="Helvetica" font-weight="900"
+                href={`http://localhost:3000/api/email?email=${email}&vote=5`}
+              >
+                5
+              </MjmlButton>
+            </MjmlColumn>
+          </MjmlGroup>
+        </MjmlSection>
+
         <MjmlSection>
           <MjmlColumn>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
+            <MjmlText
+              padding-top="50px"
+              align="center"
               color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=1`}
+              font-size="12px"
             >
-              1
-            </MjmlButton>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=2`}
-            >
-              2
-            </MjmlButton>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=3`}
-            >
-              3
-            </MjmlButton>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=4`}
-            >
-              4
-            </MjmlButton>
-            <MjmlButton
-              font-family="Helvetica"
-              background-color="#f45e43"
-              color="white"
-              href={`http://localhost:3000/api/email?email=${email}&vote=5`}
-            >
-              5
-            </MjmlButton>
+              EL Passion Next Sp. z o.o.
+              <br />
+              Grzybowska 62, 00-844 Warsaw, Poland
+              <br />{' '}
+              <a
+                href="https://www.elpassion.com/"
+                style={{color: "#00ffa3", textDecoration:"none"}}
+              >
+                Made by Monika, Klaudiusz and Krystian
+              </a>
+            </MjmlText>
           </MjmlColumn>
         </MjmlSection>
       </MjmlBody>
@@ -63,23 +193,5 @@ export function renderHtml(email: string) {
   return html;
 }
 
-import mjml2html from 'mjml';
 
-/*
-  Compile an mjml string
-*/
-const htmlOutput = mjml2html(
-  `
-  <mjml>
-    <mj-body>
-      <mj-section>
-        <mj-column>
-          <mj-text>
-            Hello World!
-          </mj-text>
-        </mj-column>
-      </mj-section>
-    </mj-body>
-  </mjml>
-`
-);
+
