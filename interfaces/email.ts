7156d070d0e56b387efb9409c1112f96dcd730a1
Krystian Kat <krystian.kat@protonmail.com>
2022-04-04 10:03:18 +0200

    feat: implement IGetEmailAddress

--- interfaces/email.ts
+++ interfaces/email.ts
@@ -1,27 +1,23 @@
-export interface IEmail {
+export interface IEmailAddress {
   to: string;
 }
 
-// export interface ISendEmail {
-//   send(to?: ISendEmail): Promise<any>;
-// }
-// export class SendEmail implements ISearchMethod {
-//   async search(request: ISendEmail): Promise<Results> {
-//     console.log('SearchMethod - request', request);
-//     // try {
-//     const response = await fetch(
-//       'http://localhost:3000/api/main?' +
-//         new URLSearchParams({
-//           send: request.to || '',
-//         })
-//     );
-//     const data = await response.json();
-//     console.log('SearchMethod data', data);
+export interface IGetEmailAddress {
+  send(to?: IEmailAddress): Promise<any>;
+}
+export class GetEmailAddress implements IGetEmailAddress {
+  async send(request: IEmailAddress): Promise<any> {
+    console.log('SearchMethod - request', request);
+    // try {
+    const response = await fetch(
+      'http://localhost:3000/api/email?' +
+        new URLSearchParams({
+          send: request.to || '',
+        })
+    );
+    const data = await response.json();
+    console.log('SearchMethod data', data);
 
-//     // return await response.json();
-//     return data;
-//     // } catch (err) {
-//     //   console.error(err);
-//     // }
-//   }
-// }
+    return data;
+  }
+}
