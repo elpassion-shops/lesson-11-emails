9d58b5e01cc51432318223c62e2f42def6efcf9a
Klaudiusz <d1f1.6@protonmail.com>
2022-04-04 10:18:49 +0200

    Endpoint /api/email handles POST request.

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,9 +1,33 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
+import * as sendgrid from "@sendgrid/mail";
+
+const key = process.env.SENDGRID_API_KEY;
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    res.status(200).json(req.query)
+    sendgrid.setApiKey(key as string)
+    if(req.method === 'POST') {
+        const msg = {
+            to: req.body.to,
+            from: 'klaudiusz.witt@gmail.com',
+            subject: 'BrrBrrBru',
+            test: 'BrrBrrBru',
+            html: '<h1>BrrBrrBru</h1>' +
+                '<p>YoyoYoYou</p>',
+        }
+        sendgrid.send(msg).then(() => {
+            res.status(200).json({
+                message: 'Email sent'
+            })
+        }).catch(err => {
+            res.status(500).json({
+                message: 'Email not sent',
+                error: err,
+                key: key,
+            })
+        })
+    }
 }
