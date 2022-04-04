571292c8e745633c642324789881949415bd852e
Krystian Kat <krystian.kat@protonmail.com>
2022-04-04 10:53:54 +0200

    feat: change GetEmailAdress method to post

--- interfaces/email.ts
+++ interfaces/email.ts
@@ -5,19 +5,63 @@ export interface IEmailAddress {
 export interface IGetEmailAddress {
   send(to?: IEmailAddress): Promise<any>;
 }
+
 export class GetEmailAddress implements IGetEmailAddress {
   async send(request: IEmailAddress): Promise<any> {
     console.log('SearchMethod - request', request);
     // try {
-    const response = await fetch(
-      'http://localhost:3000/api/email?' +
-        new URLSearchParams({
-          send: request.to || '',
-        })
-    );
-    const data = await response.json();
-    console.log('SearchMethod data', data);
-
-    return data;
+    const response = await fetch('http://localhost:3000/api/email?', {
+      method: 'POST',
+      body: new URLSearchParams({
+        send: request.to || '',
+      }),
+    });
   }
 }
+
+// export class GetEmailAddress implements IGetEmailAddress {
+//   async send(request: IEmailAddress): Promise<any> {
+//     var formBody: any = [];
+//     var encodedKey = encodeURIComponent(request);
+//     var encodedValue = encodeURIComponent(request.to);
+//     formBody.push(encodedKey + '=' + encodedValue);
+
+//     formBody = formBody.join('&');
+
+//     console.log('SearchMethod - request', request);
+
+//     const response = await fetch('http://localhost:3000/api/email?', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+//       },
+//       body: formBody,
+//     });
+//     const data = await response.json();
+//     console.log('SearchMethod data', data);
+
+//     return data;
+//   }
+// }
+
+// var details = {
+//   userName: 'test@gmail.com',
+//   password: 'Password!',
+//   grant_type: 'password',
+// };
+
+// var formBody = [];
+// for (var property in details) {
+//   var encodedKey = encodeURIComponent(property);
+//   var encodedValue = encodeURIComponent(details[property]);
+//   formBody.push(encodedKey + '=' + encodedValue);
+// }
+// formBody = formBody.join('&');
+
+// fetch('https://example.com/login', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+//   },
+//   body: formBody,
+// });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,82 @@
-import { Card, CardContent, TextField, CardActions, Button, Container, Box } from '@mui/material'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
+import {
+  Card,
+  CardContent,
+  TextField,
+  CardActions,
+  Button,
+  Container,
+  Box,
+} from '@mui/material';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { ChangeEvent, useState } from 'react';
+import { GetEmailAddress } from '../interfaces/email';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const[email, setEmail] = useState<string>("")
+  const [email, setEmail] = useState<string>('');
+
+  const emailAddressChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const submit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    new GetEmailAddress().send({ to: email });
+
+    setEmail('');
+  };
+
+  // useEffect(() => {
+  //   new SearchMethod().search({ search: enteredSearch }).then((response) => {
+  //     console.log('response', response);
+  //   });
+  // }, [enteredSearch]);
 
   return (
-    <Container maxWidth="sm" style={{marginTop: '16px'}}>
-    <Card sx={{ minWidth: 275 }} >
-    <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          p: 1,
-          m: 1,
-          bgcolor: 'background.paper',
-          borderRadius: 1,
-        }}
-      > 
-    <CardContent>
-    <TextField fullWidth label="e-mail" id="fullWidth" />
-    </CardContent>
-    </Box>
-    <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          p: 1,
-          m: 1,
-          bgcolor: 'background.paper',
-          borderRadius: 1,
-        }}>
-    <Button variant="contained" disableElevation>
-      Submit
-    </Button>
-       </Box>
-  </Card>
-  </Container>
+    <Container maxWidth="sm" style={{ marginTop: '16px' }}>
+      <Card sx={{ minWidth: 275 }}>
+        <form onSubmit={submit}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              p: 1,
+              m: 1,
+              bgcolor: 'background.paper',
+              borderRadius: 1,
+            }}
+          >
+            <CardContent>
+              <TextField
+                onChange={emailAddressChangeHandler}
+                fullWidth
+                label="e-mail"
+                id="fullWidth"
+                value={email}
+              />
+            </CardContent>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              p: 1,
+              m: 1,
+              bgcolor: 'background.paper',
+              borderRadius: 1,
+            }}
+          >
+            <Button variant="contained" type="submit" disableElevation>
+              Submit
+            </Button>
+          </Box>
+        </form>
+      </Card>
+    </Container>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
