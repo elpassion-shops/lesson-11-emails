99113e36101168f54d780b29ad317417228e8319
Klaudiusz <d1f1.6@protonmail.com>
2022-04-04 12:46:18 +0200

    Adding votes to database added.

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 import nodemailerSendgrid from 'nodemailer-sendgrid';
-import {IEmailMsg} from "../../interfaces/email";
+import {IEmailMsg, IEmailVote} from "../../interfaces/email";
 import {config} from '../../config/configuration';
+import { Pool, Client } from 'pg';
+
 
 
 const sendEmail = async (msg: IEmailMsg) => {
@@ -18,10 +20,40 @@ export default async function handler(
     res: NextApiResponse
 ) {
     if(req.method === 'GET') {
-        const data = {
-            email: req.query.email,
-            vote: req.query.vote,
+        const data: IEmailVote = {
+            email: req.query.email as string,
+            vote: Number(req.query.vote),
         }
+        const client = new Client({
+            user: 'postgres',
+            host: 'localhost',
+            database: 'postgres',
+            password: 'secret',
+            port: 5432,
+        });
+        await client.connect();
+        // Create table if not exists
+        await client.query(`CREATE TABLE IF NOT EXISTS votes (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(255) NOT NULL,
+            vote INT NOT NULL
+        )`);
+        // Check if email didnt vote before if no insert new vote
+        const result = await client.query(`SELECT * FROM votes WHERE email = $1`, [data.email]);
+        if(result.rows.length === 0) {
+            await client.query(`INSERT INTO votes (email, vote) VALUES ($1, $2)`, [data.email, data.vote]);
+        } else {
+            await client.query(`UPDATE votes SET vote = $1 WHERE email = $2`, [data.vote, data.email]);
+            res.status(400).json({
+                error: 'You already voted, and your vote changed',
+            });
+            return;
+        }
+        // Get all votes
+        const votes = await client.query(`SELECT * FROM votes`);
+        await client.end();
+        console.log(`Email: ${data.email} vote: ${data.vote}`);
+        res.status(200).json(votes);
     }
     if(req.method === 'POST') {
         const msg: IEmailMsg = {
