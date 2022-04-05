61a7651bab916492da31ea702fa5b1f23cceed6e
Krystian Kat <krystian.kat@protonmail.com>
2022-04-06 00:08:34 +0200

    test: add tests for AnswerClose

--- test/jest/api-kkr.test.ts
+++ test/jest/api-kkr.test.ts
@@ -70,3 +70,76 @@ describe('tests for AnswerOpen', () => {
     });
   });
 });
+
+describe('tests for AnswerClose', () => {
+  class AnswerClose implements IAnswer {
+    @IsBoolean()
+    isOpen!: boolean;
+
+    @Min(1)
+    @Max(5)
+    @IsInt()
+    value!: number;
+  }
+
+  function generateAnswerClose(overrides: { isOpen?: any; value?: any }) {
+    const answerClose = new AnswerClose();
+    answerClose.value = overrides.value === undefined ? 5 : overrides.value;
+    answerClose.isOpen =
+      overrides.isOpen === undefined ? true : overrides.isOpen;
+    return answerClose;
+  }
+
+  const answerCloseValid = generateAnswerClose({});
+
+  describe('tests for AnswerClose.value', () => {
+    test('should pass with correct number in value', async () => {
+      expect(await validate(answerCloseValid)).toEqual([]);
+    });
+    test('should fails with to low value', async () => {
+      const answerCloseInvalid = generateAnswerClose({ value: 0 });
+      expect(await validate(answerCloseInvalid)).toMatchSnapshot();
+    });
+
+    test('should fails with to hight value', async () => {
+      const answerCloseInvalid = generateAnswerClose({ value: 6 });
+      expect(await validate(answerCloseInvalid)).toMatchSnapshot();
+    });
+
+    test('should fails with empty value', async () => {
+      const answerCloseInvalid = generateAnswerClose({ value: null });
+      expect(await validate(answerCloseInvalid)).toMatchSnapshot();
+    });
+
+    test('should fails with number in value', async () => {
+      const answerCloseInvalid = generateAnswerClose({ value: 9 });
+      expect(await validate(answerCloseInvalid)).toMatchSnapshot();
+    });
+
+    test('should fails with boolean in value', async () => {
+      const answerCloseInvalid = generateAnswerClose({ value: true });
+      expect(await validate(answerCloseInvalid)).toMatchSnapshot();
+    });
+  });
+
+  describe('tests for AnswerClose.isOpen', () => {
+    test('should pass with boolean in isOpen', async () => {
+      expect(await validate(answerCloseValid)).toEqual([]);
+    });
+
+    test('should fails with empty isOpen', async () => {
+      const answerCloseInvalid = generateAnswerClose({ isOpen: null });
+      expect(await validate(answerCloseInvalid)).toMatchSnapshot();
+    });
+
+    test('should fails with string in isOpen', async () => {
+      const answerCloseInvalid = generateAnswerClose({ isOpen: 'abc' });
+      expect(await validate(answerCloseInvalid)).toMatchSnapshot();
+    });
+
+    test('should fails with number in isOpen', async () => {
+      const answerCloseInvalid = generateAnswerClose({ isOpen: 9 });
+      expect(await validate(answerCloseInvalid)).toMatchSnapshot();
+    });
+  });
+});
