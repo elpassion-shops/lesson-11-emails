379da9dd9d39ef372a4cd37e4eb73d57e263bb21
Klaudiusz <d1f1.6@protonmail.com>
2022-04-05 15:17:06 +0200

    Interface questionnaire added

---
+++ interfaces/questionnaire.ts
@@ -0,0 +1,20 @@
+export interface Question {
+  id: number; // 0 - n
+  name?: string; // aA-zZ
+  ratings?: number[]; // 0 - 5|| 0 - n
+  choice: number | null; // ratings.includes(choice)
+}
+
+export interface IQuestionnaire {
+  id: number; // 0 - n,
+  title: string; // aA-zZ
+  email?: string | null; // email address
+  question: Question[]; // every question has choice selected => if(question[n].choice) != null => true.
+}
+export interface IQuestionnaireResponse {
+  questionnaire: IQuestionnaire;
+}
+
+export interface IQuestionnaireRequest {
+  id: string;
+}
