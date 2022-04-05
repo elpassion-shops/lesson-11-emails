7794207dbd9b69e026d914cbe761753acda2a3b0
Michał Warda <michal.warda@elpassion.pl>
2022-04-05 15:56:10 +0200

    Handle failing email field

--- test/validate-request.test.ts
+++ test/validate-request.test.ts
@@ -1,10 +1,23 @@
-import {validate} from "class-validator";
+import {IsEmail, IsString, validate} from "class-validator";
 
 class Request {
-
+  @IsString()
+  @IsEmail()
+  email!: string;
 }
 
-test('passes with correct request', async ()=>{
+function generateRequest(overrides: { email?: any }) {
   const request = new Request();
+  request.email = overrides.email === undefined ? 'mihau@gmail.com' : overrides.email
+  return request
+}
+
+test('passes with correct request', async () => {
+  const request = generateRequest({});
   expect(await validate(request)).toEqual([])
+})
+
+test('fails without email', async () => {
+  const request = generateRequest({ email: null });
+  expect(await validate(request)).toMatchSnapshot()
 })
