a7027b84f48318be6992dd72d03f1fa012d6133d
Krystian Kat <krystian.kat@protonmail.com>
2022-04-05 23:21:32 +0200

    refactor: remove null from IQuestion and IQuestionnaire

--- interfaces/questionnaire.ts
+++ interfaces/questionnaire.ts
@@ -1,20 +1,50 @@
+export interface IAnswer {
+  isOpen: boolean;
+  value: string | number;
+}
+
 export interface IQuestion {
-  id: number; // 0 - n
-  name?: string; // aA-zZ
-  ratings?: number[]; // 0 - 5|| 0 - n
-  choice: number | null; // ratings.includes(choice)
+  id: number;
+  question: string;
+  answer?: IAnswer | null;
 }
 
 export interface IQuestionnaire {
-  id: number; // 0 - n,
-  title: string; // aA-zZ
-  email?: string | null; // email address
-  question: IQuestion[]; // every question has choice selected => if(question[n].choice) != null => true.
+  id: number;
+  title: string;
+  email?: string | null;
+  questions: IQuestion[];
 }
+
 export interface IQuestionnaireResponse {
   questionnaire: IQuestionnaire;
 }
 
 export interface IQuestionnaireRequest {
   id: string;
+}
+
+abstract class Answer implements IAnswer {
+  abstract isOpen: boolean;
+  constructor(public value: string | number) {}
+}
+
+export class AnswerOpen extends Answer {
+  isOpen = true;
+  value: string;
+
+  constructor(value: string) {
+    super(value);
+    this.value = value;
+  }
+}
+
+export class AnswerClose extends Answer {
+  isOpen = false;
+  value = 0;
+
+  constructor(value: number) {
+    super(value);
+    this.value = value;
+  }
 }
