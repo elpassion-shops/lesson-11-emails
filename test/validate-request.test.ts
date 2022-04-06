deb5db2be34b3983fc61c39347d8cfc5a090f621
Michał Warda <michal.warda@elpassion.pl>
2022-04-06 09:31:04 +0200

    Replace snapshots with partial matchers

--- test/validate-request.test.ts
+++ test/validate-request.test.ts
@@ -1,7 +1,6 @@
-import {IsEmail, IsString, validate} from "class-validator";
+import { IsEmail, IsString, validate } from "class-validator";
 
-class Answer {
-}
+class Answer {}
 
 class Request {
   @IsString()
@@ -12,30 +11,53 @@ class Request {
   answers!: string[];
 }
 
-function generateRequest(overrides: { email?: any, answers?: any }) {
+function generateRequest(overrides: { email?: any; answers?: any }) {
   const request = new Request();
-  request.email = overrides.email === undefined ? 'mihau@gmail.com' : overrides.email
-  request.answers = overrides.answers === undefined ? [] : overrides.answers
+  request.email =
+    overrides.email === undefined ? "mihau@gmail.com" : overrides.email;
+  request.answers = overrides.answers === undefined ? [] : overrides.answers;
 
-  return request
+  return request;
 }
 
-test('passes with correct request', async () => {
+test("passes with correct request", async () => {
   const request = generateRequest({});
-  expect(await validate(request)).toEqual([])
-})
+  expect(await validate(request)).toEqual([]);
+});
 
-test('fails without email', async () => {
+test("fails without email", async () => {
   const request = generateRequest({ email: null });
-  expect(await validate(request)).toMatchSnapshot()
-})
+  expect(await validate(request)).toEqual([
+    expect.objectContaining({
+      constraints: {
+        isEmail: "email must be an email",
+        isString: "email must be a string",
+      },
+      property: "email",
+    }),
+  ]);
+});
 
-test('fails with incorrect email', async () => {
-  const request = generateRequest({ email: 'mihaugmail.com' });
-  expect(await validate(request)).toMatchSnapshot()
-})
+test("fails with incorrect email", async () => {
+  const request = generateRequest({ email: "mihaugmail.com" });
+  expect(await validate(request)).toEqual([
+    expect.objectContaining({
+      constraints: {
+        isEmail: "email must be an email",
+      },
+      property: "email",
+    }),
+  ]);
+});
 
-test('fails with empty answers', async () => {
+test("fails with empty answers", async () => {
   const request = generateRequest({ answers: null });
-  expect(await validate(request)).toMatchSnapshot()
-})
+  expect(await validate(request)).toEqual([
+    expect.objectContaining({
+      constraints: {
+        isString: "each value in answers must be a string",
+      },
+      property: "answers",
+    }),
+  ]);
+});
