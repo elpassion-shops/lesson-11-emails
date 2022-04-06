c108c83f3e717e651c6e2094b10b197dfc6969dd
Klaudiusz <d1f1.6@protonmail.com>
2022-04-06 13:58:38 +0200

    Endpoint #POST questionnaries/[pid] added.

--- pages/api/questionnaries/[pid].ts
+++ pages/api/questionnaries/[pid].ts
@@ -1,16 +1,15 @@
+import "reflect-metadata";
 import { NextApiRequest, NextApiResponse } from "next";
-import * as QueryString from "querystring";
 import {
-  AnswerClose,
   IAnswer,
   IQuestion,
   IQuestionnaire,
   IQuestionnaireResponse,
-  Question,
-  Questionnaire,
 } from "../../../interfaces/questionnaire";
 import { GetQuestionnairesDto } from "../../../dto/get_questionnaries.dto";
 import { validateOrReject } from "class-validator";
+import { SendQuestionnariesDto } from "../../../dto/send_questionnaries.dto";
+import { plainToInstance } from "class-transformer";
 
 export default async function handler(
   req: NextApiRequest,
@@ -33,13 +32,10 @@ export default async function handler(
       questions: [question],
     };
     const questionnaireResponse: IQuestionnaireResponse = {
-      questionnaire: questionnaire,
+      questionnaireResponse: questionnaire,
     };
     const getQuestionnaire = new GetQuestionnairesDto();
     getQuestionnaire.id = Number(pid);
-    validateOrReject(getQuestionnaire).catch((e) => {
-      console.log("Promise rejected (validation failed). Errors: ", e);
-    });
     try {
       await validateOrReject(getQuestionnaire);
       res.status(201).json({ questionnaireResponse });
@@ -48,14 +44,12 @@ export default async function handler(
     }
   }
   if (req.method === "POST") {
-    const answer = new AnswerClose(3);
-    const question1 = new Question(1, "Rate bootcamp, please!", answer);
-    const questionnaire1 = new Questionnaire(1, "Test questionnaire", []);
     try {
-      await validateOrReject(questionnaire1);
-      res.status(20).json({ questionnaire1 });
+      const pTC = plainToInstance(SendQuestionnariesDto, req.body);
+      await validateOrReject(pTC);
+      res.status(200).json({});
     } catch (e: any) {
-      res.status(500).json({ errorArray: e[0].constraints });
+      res.status(400).json({ errorArray: e });
     }
   }
 }
