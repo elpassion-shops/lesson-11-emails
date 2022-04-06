d3f193fe74069980370cd4806adcbef5259ed776
Klaudiusz <d1f1.6@protonmail.com>
2022-04-06 10:14:23 +0200

    New api interfaces implementation added.

--- test/jest/api.ts
+++ test/jest/api.ts
@@ -1,7 +1,2 @@
-import 'class-validator';
-import { IApi } from '../../interfaces/email';
-import { IQuestionnaireResponse } from '../../interfaces/questionnaire';
-
-class Api implements IApi {
-  getQuestionnaire(id: string): Promise<IQuestionnaireResponse> {}
-}
+import "class-validator";
+import { IQuestionnaireResponse } from "../../interfaces/questionnaire";
